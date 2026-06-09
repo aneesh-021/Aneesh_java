30b5eaf60095715f9462b4499ee9c4cf82630462
Aneesh Desai <desaianeesh2004@gmail.com>
2026-06-09 05:48:44 +0530

    Todays java practice

---
+++ Patterns/rectangle.java
@@ -0,0 +1,23 @@
+package Patterns;
+import java.util.*;
+
+public class rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int row = sc.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int col = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
